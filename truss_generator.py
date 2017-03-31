10e76a58e71f11346e37500480ff0e49b7645816
furkanuyar <furkanuyar92@gmail.com>
2017-03-31 22:45:20 +0300

    truss generator is reviewed and so looked at.

--- truss_generator.py
+++ truss_generator.py
@@ -22,21 +22,20 @@ def Truss_Generator(seed):
     # Random Truss Generator
     Trusses = []
     rnd.seed(seed)
-    for i in range(pc):
-        h1 = rnd.randrange(h1_min,h1_max,step)
+    for _ in range(pc):
+        h1 = rnd.randrange(h1_min, h1_max, step)
         h2 = rnd.randrange(h2_min, h2_max, step)
-        n_div = rnd.randrange(n_min, n_max, 1)
-        div_inf = [rnd.randrange(0,3,1) for j in range(n_div)]
-        Trusses.append(Truss({"h1": h1, "h2": h2, "l": l,
-                              "n_div": n_div, "div_inf": div_inf}))
+        n_div = rnd.randrange(n_min, n_max)
+        div_inf = [rnd.randrange(3) for _ in range(n_div)]
+        Trusses.append(Truss({"h1": h1, "h2": h2, "l": l, "n_div": n_div, "div_inf": div_inf}))
 
     lines = []
     cnt = 1
     offset = 3000
-    for i in Trusses:
+    for truss in Trusses:
         off = offset * cnt
-        i.plot_truss(off)
-        lines += i.lines
+        truss.plot_truss(off)
+        lines += truss.lines
         cnt += 1
 
     lc = mpl.collections.LineCollection(lines, linewidths=2)
@@ -46,142 +45,145 @@ def Truss_Generator(seed):
     ax.margins(1)
     input()
 
-
     return Trusses
 
+
 class Truss:
-        # Truss Parameters for DNA Creation
-        # h1, h2, l (in mm)
-        # n_div (integer)
-        def __init__(self, param):
-            self.h1 = param["h1"]
-            self.h2 = param["h2"]
-            self.l = param["l"]
-            self.n_div = param["n_div"]
-            div_inf = param["div_inf"]
-            self.node_cr()
-            self.elem_cr(div_inf)
+    # Truss Parameters for DNA Creation
+    # h1, h2, l (in mm)
+    # n_div (integer)
+    def __init__(self, param):
+        self.h1 = param["h1"]
+        self.h2 = param["h2"]
+        self.l = param["l"]
+        self.n_div = param["n_div"]
+        div_inf = param["div_inf"]
+        self.node_cr()
+        self.elem_cr(div_inf)
 
-        def node_cr(self):
-            # Creation of Nodal Information
+    def node_cr(self):
+        # Creation of Nodal Information
 
-            top_angle = m.atan((self.h2 - self.h1) * 2 / self.l)
-            step_x = self.l / self.n_div / 2
-            step_z = (self.h2 - self.h1) / self.n_div
+        top_angle = m.atan((self.h2 - self.h1) * 2 / self.l)
+        step_x = self.l / self.n_div / 2
+        step_z = (self.h2 - self.h1) / self.n_div
 
-            nodes = []
+        nodes = []
 
-            x = 0
-            n = 1
-            for i in range(2 * self.n_div + 1):
-                nodes.append([x, 0])
-                n += 1
-                x += step_x
+        x = 0
+        n = 1
+        for _ in range(2 * self.n_div + 1):
+            nodes.append([x, 0])
+            n += 1
+            x += step_x
 
-            z = self.h1
-            x = 0
-            for i in range(self.n_div + 1):
-                nodes.append([x, z])
-                z += step_z
-                x += step_x
-                n += 1
+        z = self.h1
+        x = 0
+        for _ in range(self.n_div + 1):
+            nodes.append([x, z])
+            z += step_z
+            x += step_x
+            n += 1
 
-            z -= 2 * step_z
-            for i in range(0, self.n_div):
-                nodes.append([x, z])
-                z -= step_z
-                x += step_x
-                n += 1
+        z -= 2 * step_z
+        for _ in range(0, self.n_div):
+            nodes.append([x, z])
+            z -= step_z
+            x += step_x
+            n += 1
 
-            self.n_coord = nodes
-            self.node_count = n
+        self.n_coord = nodes
+        self.node_count = n
 
-        def elem_cr(self, div_inf):
-            # Creation of Truss Elements
+    def elem_cr(self, div_inf):
+        # Creation of Truss Elements
 
-            # Truss Top and Bottom Chords
-            n = 1
-            elem = []
-            for j in range(2):
-                for i in range(2 * self.n_div):
-                    node_i = n
-                    node_j = n + 1
-                    elem.append([node_i, node_j])
-                    n += 1
-                n += 1
-
-            # Truss Posts
-            n = 1
-            for i in range(2 * self.n_div + 1):
+        # Truss Top and Bottom Chords
+        n = 1
+        elem = []
+        for j in range(2):
+            for _ in range(2 * self.n_div):
                 node_i = n
-                node_j = 2 * self.n_div + 1 + n
+                node_j = n + 1
                 elem.append([node_i, node_j])
                 n += 1
+            n += 1
 
-            # Truss Diagonals
-            left = div_inf
-            right = []
-            for i in div_inf[::-1]:
-                if i == 1:
-                    right.append(0)
-                elif i == 0:
-                    right.append(1)
-                else:
-                    right.append(i)
+        # Truss Posts
+        n = 1
+        for _ in range(2 * self.n_div + 1):
+            node_i = n
+            node_j = 2 * self.n_div + 1 + n
+            elem.append([node_i, node_j])
+            n += 1
 
-            self.dia_orient = left + right
+        # Truss Diagonals
+        left = div_inf
+        right = []
+        for i in div_inf[::-1]:
+            if i == 1:
+                right.append(0)
+            elif i == 0:
+                right.append(1)
+            else:
+                right.append(i)
 
-            n = 1
-            for i in range(2 * self.n_div):
-                orient = self.dia_orient[i]
+        self.dia_orient = left + right
 
-                if orient == 1:
-                    node_i = n + 2 * self.n_div + 1
-                    node_j = n + 1
-                    elem.append([node_i, node_j])
-                elif orient == 0:
-                    node_i = n
-                    node_j = n + 2 * self.n_div + 2
-                    elem.append([node_i, node_j])
-                else:
-                    node_i = n + 2 * self.n_div + 1
-                    node_j = n + 1
-                    elem.append([node_i, node_j])
-                    node_i = n
-                    node_j = n + 2 * self.n_div + 2
-                    elem.append([node_i, node_j])
-                n += 1
-            self.elements = elem
+        n = 1
+        for _ in range(2 * self.n_div):
+            orient = self.dia_orient[i]
 
-        def plot_truss(self, offset):
-            lines = []
-            for i in self.elements:
-                node_i = i[0]
-                node_j = i[1]
-                coord_ix = self.n_coord[node_i - 1][0]
-                coord_iz = self.n_coord[node_i - 1][1] + offset
-                coord_jx = self.n_coord[node_j - 1][0]
-                coord_jz = self.n_coord[node_j - 1][1] + offset
-                coord_i = (coord_ix, coord_iz)
-                coord_j = (coord_jx, coord_jz)
-                lines.append([coord_i, coord_j])
-            self.lines = lines
+            if orient == 1:
+                node_i = n + 2 * self.n_div + 1
+                node_j = n + 1
+                elem.append([node_i, node_j])
+            elif orient == 0:
+                node_i = n
+                node_j = n + 2 * self.n_div + 2
+                elem.append([node_i, node_j])
+            else:
+                node_i = n + 2 * self.n_div + 1
+                node_j = n + 1
+                elem.append([node_i, node_j])
+                node_i = n
+                node_j = n + 2 * self.n_div + 2
+                elem.append([node_i, node_j])
+            n += 1
+        self.elements = elem
 
-        def member(self):
-            pass
+    def plot_truss(self, offset):
+        lines = []
+        for element in self.elements:
+            node_i = element[0]
+            node_j = element[1]
+            coord_ix = self.n_coord[node_i - 1][0]
+            coord_iz = self.n_coord[node_i - 1][1] + offset
+            coord_jx = self.n_coord[node_j - 1][0]
+            coord_jz = self.n_coord[node_j - 1][1] + offset
+            coord_i = (coord_ix, coord_iz)
+            coord_j = (coord_jx, coord_jz)
+            lines.append([coord_i, coord_j])
+        self.lines = lines
+
+    def member(self):
+        pass
+
 
 class Steel:
-        def __init__(self, fy, fu, e, gamma, alpha):
-            self.Fy = fy
-            self.Fu = fu
-            self.E = e
-            self.gamma = gamma
-            self.alpha = alpha
+    def __init__(self, fy, fu, e, gamma, alpha):
+        self.Fy = fy
+        self.Fu = fu
+        self.E = e
+        self.gamma = gamma
+        self.alpha = alpha
+
 
 # Steel Catalogue
-catalogue = {"S235":Steel(235,360,200000,7850,1.17e-5), "S275": Steel(275,430,200000,7850,1.17e-5),
-"S355": Steel(355,510,200000,7850,1.17e-5), "S450": Steel(440,550,200000,7850,1.17e-5)}
-
+catalogue = {"S235": Steel(235, 360, 200000, 7850, 1.17e-5),
+             "S275": Steel(275, 430, 200000, 7850, 1.17e-5),
+             "S355": Steel(355, 510, 200000, 7850, 1.17e-5),
+             "S450": Steel(440, 550, 200000, 7850, 1.17e-5)}
 
 # Input Card
 
@@ -202,7 +204,10 @@ mp = 1  # Mutation Probability
 # Material and Section Properties
 mat = catalogue["S275"]  # Available Material Properties are S235, S275, S355 and S450
 
-#- W for wide flange,
+# Loading Information
+SDL = 500  # kgf/m2
+
+# - W for wide flange,
 # O for CHS,
 # K for RHS & SHS,
 # L for Angle
@@ -214,24 +219,4 @@ tc_mem = "W"  # Preferred Sectional Shape for Truss Chords For Now only W sectio
 tp_mem = "O"  # Preferred Sectional Shape for Truss Posts - For Now only O, K, C, L sections available
 td_mem = "O"  # Preferred Sectional Shape for Truss Posts - For Now only O, K, C, L sections available
 
-
 Truss_Generator(2)
-
-#class Member:
-#    def __init__(self):
-
-
-# Structural Analysis
-
-# for i in Tru:
-
-
-
-
-
-
-
-
-
-
-
